f6305e1f353b1ed8035ab9670f33545c8b4a2468
Mike Schonebaum <schmike@rams.colostate.edu>
2019-11-14 22:23:57 -0700

    Initial Commit

---
+++ joyRide_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  joyRide_1
+//
+//  Created by Mike Schonebaum on 11/14/19.
+//  Copyright © 2019 Mike Schonebaum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
